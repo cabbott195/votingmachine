31061d51fdb58617d29f75184797e725c84dcb32
Colby <cabbott195@west-mec.org>
2021-04-21 18:22:17 -0700

    Fixed CSS, added more functions.

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-// let img = document.createElement('img');
-// img.src = "https://js.cx/clipart/train.gif"; // (*)
-// object.addEventListener("load", img);
-
-// let screenLog = document.getElementsByClassName('mcg');
-// document.addEventListener('mousemove', mcg);
-
-// function mcg(e) {
-//   screenLog.innerText = `
-//     Screen X/Y: ${e.screenX}, ${e.screenY}
-//     Client X/Y: ${e.clientX}, ${e.clientY}`;
-// }
-
-// (function blink() {
-//     $('hypno').fadeOut(500).fadeIn(500, blink);
-//   })();
-
-// $(function() {
-//     var img = $("#dababy"),
-//         width = img.get(0).width,
-//         screenWidth = $(window).width(),
-//         duration = 5000;
-
-//         function animateDababy() {
-//             img.css("left", -width).animate({
-//                 "left": screenWidth
-//             }, duration, animateDababy);
-//         }
-
-//         animateDababy();
-// });
-
-// object.addEventListener("click", animateDababy);
+//My onload function
+window.onload = function() {
+    let socialPrompt = prompt ("If you found a bag of money on the ground and your social security number was the amount that you found, how much money would you have?");
+    student["ssn"] = socialPrompt;
+}
+//Onclick functions
 function myWindow(){
     myWindow = window.open("patrick2.mp4", "width=150,height=1500");
 }
 
-function User(name, id){
-    this.name = "Colby";
-
-    console.log(this.name)
+function myWindow2(){
+    myWindow2 = window.open("dababy.mp4", "width=150,height=1500");
 }
+
+// function User(name, id){
+//     this.name = "Colby";
+
+//     console.log(this.name)
+// }
+
+// //"This" Function
+// const counter = {
+//     count: 0,
+//     next: function () {
+//         return ++this.count;
+//     }
+// };
+
+// counter.next();
+
+var myDiv = document.getElementById('mcg');
+
+myDiv.onmouseover = function() { 
+    alert('entered');
+}
+
+myDiv.onmouseout = function() { 
+    alert('left');
+}
+
+object.addEventListener("mouseover", myScript);
